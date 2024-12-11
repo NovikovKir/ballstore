375b44d63d718a0df97d5f2a0d53c99db5bdbea2
Кирилл Новиков <novik27.06@mail.ru>
2024-12-11 22:13:25 +0700

    web

---
+++ presentation/BallStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BallStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
