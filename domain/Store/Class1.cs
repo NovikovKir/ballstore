e211f90d4e825f4fbd9c0e9811ebb75fbdc1aa27
Кирилл Новиков <novik27.06@mail.ru>
2024-12-11 21:36:35 +0700

    commit

---
+++ domain/Store/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Store;
+
+public class Balls
+{
+    public int ID { get; }
+
+
+    public string Name { get; }
+
+    public Balls(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+}
