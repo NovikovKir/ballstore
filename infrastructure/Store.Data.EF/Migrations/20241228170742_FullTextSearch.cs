06e05dc46bd93163c8e5b59a6c2832c18e525821
Кирилл Новиков <novik27.06@mail.ru>
2024-12-29 01:05:26 +0700

    Добавлен полнотекстовый поиск

---
+++ infrastructure/Store.Data.EF/Migrations/20241228170742_FullTextSearch.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class FullTextSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG BallStoreFullTextCatalog AS DEFAULT", suppressTransaction: true);
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON Balls(Brand, Model) KEY INDEX PK_Balls WITH STOPLIST = SYSTEM", suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Balls", suppressTransaction: true);
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG StoreFullTextCatalog", suppressTransaction: true);
+        }
+    }
+}
